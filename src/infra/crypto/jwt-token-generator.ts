fe657cc00efb8b802455884e54608a5d1c4b3197
Antonny Mello <antonny.mello@gmail.com>
2023-03-31 15:17:40 -0300

    refactor: move JwtTokenGenerator to its own file

---
+++ src/infra/crypto/jwt-token-generator.ts
@@ -0,0 +1,17 @@
+import { sign } from "jsonwebtoken";
+
+import { type TokenGenerator } from "@/data/contracts/crypto";
+
+export class JwtTokenGenerator {
+  constructor(private readonly secret: string) {}
+
+  async generateToken(
+    params: TokenGenerator.Params
+  ): Promise<TokenGenerator.Result> {
+    const expirationInSeconds = params.expirationInMs / 1000;
+
+    return sign({ key: params.key }, this.secret, {
+      expiresIn: expirationInSeconds,
+    });
+  }
+}
