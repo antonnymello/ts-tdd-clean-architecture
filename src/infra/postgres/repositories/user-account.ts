ee605cb972104a2f566a7c49fe5d3c536ccd357a
Antonny Mello <antonny.mello@gmail.com>
2023-04-06 15:16:41 -0300

    ensure PostgresUserAccountRepository creates an account if id is undefined

--- src/infra/postgres/repositories/user-account.ts
+++ src/infra/postgres/repositories/user-account.ts
@@ -1,9 +1,14 @@
+/* eslint-disable @typescript-eslint/brace-style */
 import { getRepository } from "typeorm";
 import { PostgresUser } from "@/infra/postgres/entities";
-import { type LoadUserAccountRepository } from "@/data/contracts/repositories";
+import {
+  type SaveFacebookAccountRepository,
+  type LoadUserAccountRepository,
+} from "@/data/contracts/repositories";
 
 export class PostgresUserAccountRepository
-  implements LoadUserAccountRepository {
+  implements LoadUserAccountRepository
+{
   async load(
     params: LoadUserAccountRepository.Params
   ): Promise<LoadUserAccountRepository.Result> {
@@ -18,5 +23,16 @@ export class PostgresUserAccountRepository
       id: postgresUser.id.toString(),
       name: postgresUser.name ?? undefined,
     };
+  }
+
+  async saveWithFacebook(
+    params: SaveFacebookAccountRepository.Params
+  ): Promise<void> {
+    const postgresUserRepository = getRepository(PostgresUser);
+    await postgresUserRepository.save({
+      email: params.email,
+      name: params.name,
+      facebookId: params.facebookId,
+    });
   }
 }
