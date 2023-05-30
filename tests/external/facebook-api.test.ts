dcf078cf9dc1ebad815b036f6b1443b6f5aebfdc
Antonny Mello <antonny.mello@gmail.com>
2023-05-30 09:25:22 -0300

    refactor: isolate sut creation

--- tests/external/facebook-api.test.ts
+++ tests/external/facebook-api.test.ts
@@ -3,14 +3,19 @@ import { AxiosHttpClient } from "@/infra/http";
 import { env } from "@/main/config/env";
 
 describe("", () => {
-  it("should return a Facebook User if token is valid", async () => {
-    const axiosClient = new AxiosHttpClient();
-    const sut = new FacebookApi(
+  let sut: FacebookApi;
+  let axiosClient: AxiosHttpClient;
+
+  beforeEach(() => {
+    axiosClient = new AxiosHttpClient();
+    sut = new FacebookApi(
       axiosClient,
       env.facebookApi.clientId,
       env.facebookApi.clientSecret
     );
+  });
 
+  it("should return a Facebook User if token is valid", async () => {
     const fbUser = await sut.loadUser({ token: "valid_token" });
 
     expect(fbUser).toEqual({
@@ -21,13 +26,6 @@ describe("", () => {
   });
 
   it("should return undefined if token is invalid", async () => {
-    const axiosClient = new AxiosHttpClient();
-    const sut = new FacebookApi(
-      axiosClient,
-      env.facebookApi.clientId,
-      env.facebookApi.clientSecret
-    );
-
     const fbUser = await sut.loadUser({ token: "invalid_token" });
 
     expect(fbUser).toBeUndefined();
