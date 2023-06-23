1acccbf3995fc676d83da1e914b2f742f08c8038
Antonny Mello <antonny.mello@gmail.com>
2023-06-23 17:56:29 -0300

    feat: ensure ExpressRouterAdapter calls handle with correct request

---
+++ tests/infra/http/express-router-adapter.spec.ts
@@ -0,0 +1,25 @@
+import type { Request, Response } from "express";
+import { getMockReq, getMockRes } from "@jest-mock/express";
+import { mock } from "jest-mock-extended";
+import { type Controller } from "@/application/controllers";
+
+class ExpressRouterAdapter {
+  constructor(private readonly controller: Controller) {}
+
+  async adapt(req: Request, res: Response): Promise<void> {
+    await this.controller.handle({ ...req.body });
+  }
+}
+
+describe("ExpressRouterAdapter", () => {
+  it("should call handler with correct request", async () => {
+    const request = getMockReq({ body: { any: "any" } });
+    const { res: response } = getMockRes();
+    const controller = mock<Controller>();
+    const sut = new ExpressRouterAdapter(controller);
+
+    await sut.adapt(request, response);
+
+    expect(controller.handle).toHaveBeenCalledWith({ any: "any" });
+  });
+});
