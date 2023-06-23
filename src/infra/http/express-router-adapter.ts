e421dbdcfa90ac3f1ef4d59b89fa50a7ad35f712
Antonny Mello <antonny.mello@gmail.com>
2023-06-23 18:17:14 -0300

    refactor: move ExpressRouter to its own file

---
+++ src/infra/http/express-router-adapter.ts
@@ -0,0 +1,19 @@
+import type { Request, Response } from "express";
+
+import { type Controller } from "@/application/controllers";
+
+export class ExpressRouterAdapter {
+  constructor(private readonly controller: Controller) {}
+
+  async adapt(req: Request, res: Response): Promise<void> {
+    const httpResponse = await this.controller.handle({ ...req.body });
+
+    if (httpResponse.statusCode === 200) {
+      res.status(200).json(httpResponse.data);
+    } else {
+      res
+        .status(httpResponse.statusCode)
+        .json({ error: httpResponse.data.message });
+    }
+  }
+}
--- tests/infra/http/express-router-adapter.spec.ts
+++ tests/infra/http/express-router-adapter.spec.ts
@@ -1,23 +1,9 @@
 import type { Request, Response } from "express";
 import { getMockReq, getMockRes } from "@jest-mock/express";
 import { type MockProxy, mock } from "jest-mock-extended";
+
 import { type Controller } from "@/application/controllers";
-
-class ExpressRouterAdapter {
-  constructor(private readonly controller: Controller) {}
-
-  async adapt(req: Request, res: Response): Promise<void> {
-    const httpResponse = await this.controller.handle({ ...req.body });
-
-    if (httpResponse.statusCode === 200) {
-      res.status(200).json(httpResponse.data);
-    } else {
-      res
-        .status(httpResponse.statusCode)
-        .json({ error: httpResponse.data.message });
-    }
-  }
-}
+import { ExpressRouterAdapter } from "@/infra/http/";
 
 describe("ExpressRouterAdapter", () => {
   let request: Request;
